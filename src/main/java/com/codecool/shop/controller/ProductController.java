71faa17f19bf5b9817e777967db08fad91a8cd0d
gligoravram <gligor_avram@yahoo.com>
2022-03-28 09:51:30 +0300

    add dropdown with categories to index page

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -29,6 +29,7 @@ public class ProductController extends HttpServlet {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("categories", productCategoryDataStore.getAll());
         context.setVariable("category", productService.getProductCategory(1));
         context.setVariable("products", productService.getProductsForCategory(1));
         // // Alternative setting of the template context
