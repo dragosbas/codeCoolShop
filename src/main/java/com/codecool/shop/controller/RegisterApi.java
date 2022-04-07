f91b7a76b570b2ce06900952c9c7296277b8d802
dragosbas <89776157+dragosbas@users.noreply.github.com>
2022-04-07 11:52:16 +0300

    added basic route

---
+++ src/main/java/com/codecool/shop/controller/RegisterApi.java
@@ -0,0 +1,24 @@
+package com.codecool.shop.controller;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "registerApi", urlPatterns = "/register", loadOnStartup = 9)
+public class RegisterApi extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect(req.getContextPath() + "/card-payment");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.sendRedirect(req.getContextPath() + "/success");
+    }
+}
